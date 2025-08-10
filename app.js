09803f2cd97a37bc12b17f40af229792c68dd5b3
ShantanuJha <jha.shantanu378@gmail.com>
2025-08-11 03:27:40 +0530

    Frontend changes to files and forms

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ const mongoose = require("mongoose");
 const listing = require("./models/listing.js");
 const path = require("path");
 const methodOverride = require("method-override");
+const ejsMate = require("ejs-mate");
 
 
 main().then((res) => {
@@ -21,6 +22,8 @@ app.set("view engine","ejs");
 app.set("views",path.join(__dirname,"views"));
 app.use(express.urlencoded({extended: true}));
 app.use(methodOverride("_method"));
+app.engine('ejs', ejsMate);
+app.use(express.static(path.join(__dirname,"public")));
 
 //index route
 app.get("/listings", async(req,res) => {
